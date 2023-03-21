21429b8dcf7bd072b3122c7e1c0c0c5b67d957b4
Luis Langa <luis.langajr96@gmai.com>
2023-03-21 13:18:23 -0300

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -32,6 +32,7 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: path.resolve(__dirname,"src","database","database.db"),
+      foreign_keys: true
     },
     pool: {
       afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
